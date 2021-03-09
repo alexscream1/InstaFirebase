445b1902e0e3e3568240302b02e54adbf701a3a6
Alexey Onoprienko <oleksiyonoprienko@gmail.com>
2021-03-09 13:26:56 +0200

    2. Create add photo button, info textfields

--- InstaFirebase/ViewController.swift
+++ InstaFirebase/ViewController.swift
@@ -8,12 +8,108 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Create plusPhotoButton
+    let plusPhotoButton : UIButton = {
+        let button = UIButton(type: .system)
+        let imageButton = UIImage(named: "camera")
+        button.setImage(imageButton, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    // Email Textfield
+    
+    let emailTextField : UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.placeholder = "Email"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 14)
 
+        return tf
+    }()
+    
+    // Username Textfield
+    
+    let usernameTextField : UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.placeholder = "Username"
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+
+        return tf
+    }()
+    
+    // Password Textfield
+    
+    let passwordTextField : UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+        return tf
+    }()
+    
+    // Sign Up Button
+    
+    let signUpButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign up", for: .normal)
+        button.layer.cornerRadius = 5
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(plusPhotoButton)
+        
+        // PlusPhotoButton constraints
+        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        
+        
+        setupInputFields()
+        
     }
 
+    fileprivate func setupInputFields() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+        
+        view.addSubview(stackView)
+        
+        // Stackview constraints
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 30),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        
+    }
 
 }
 
